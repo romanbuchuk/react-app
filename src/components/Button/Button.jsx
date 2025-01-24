42f074d4e1937899a23d951d382a6f399e82a5d8
Roman Buchuk <roman.buchuk@sigmatechnology.com>
2025-01-24 16:03:01 +0200

    Added first react component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+// const Button = ({ children, ...rest }) => {
+const Button = ({ children }) => {
+    // const { children, ...rest } = props;
+
+
+    return (
+        <button >{children}</button>
+    );
+}
+
+Button.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node, PropTypes.string]),
+  };
+
+export default Button;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const domNode = document.getElementById('root');
+const rootEl = createRoot(domNode);
+
+rootEl.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
